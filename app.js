680084b0ff14eb4056de43dfb57d4b735d8e382b
Kevin Lam <kevin127lam@gmail.com>
2024-03-03 16:46:59 -0500

    added html skeleton and attempt at app code

--- app.js
+++ app.js
@@ -10,6 +10,45 @@ app.use(express.static(
 ));
 
 // Insert code here
+let books = {};
+let bookNum = 0;
+
+function returnBooks(res) {
+  let bookList = [];
+  for (b in books) {
+    let book = books[b];
+    bookList.push[b];
+  }
+  const ret = JSON.stringify(bookList);
+  res.end(ret);
+}
+
+app.get("/load", (req, res) => {
+  returnBooks(res);
+});
+
+app.get("/add", (req, res) => {
+  const title = req.query.title;
+  const auth = req.query.auth;
+  const genre = req.query.genre;
+  const publ = req.query.publ;
+  const year = req.query.year;
+  const type = req.query.type;
+  const book = new Book(title, auth, genre, publ, year, type)
+  books[bookNum++] = book;
+  returnBooks(res);
+});
+
+class Book {
+  constructor(title, author, genre, publisher, year, type) {
+    this.title = title;
+    this.author = author;
+    this.genre = genre;
+    this.publisher = publisher;
+    this.year = year;
+    this.type = type;
+  }
+}
 
 app.listen(3000);
 
